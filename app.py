da054ae52b4d2c26af3e4b80e4f4fa9a2f788d37
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2018-12-16 17:44:28 +0100

    added regex check for email in form input and redirects for when check fails

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import os
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+import re
 import os
 app = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv('DATABASE_URL'):
     raise RuntimeError("DATABASE_URL is not set")
+
+EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -27,3 +30,16 @@ def index():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/handle-registration", methods=["POST"])
+def register_user():
+    email = request.form.get("email")
+    if not EMAIL_REGEX.match(email):
+        return redirect(url_for('submission_error', message="Invalid email!"))
+    user_name = request.form.get("user-name")
+    password = request.form.get("pass")
+    return render_template("home.html")
+
+@app.route("/submission_error/<string:message>")
+def submission_error(message):
+    return render_template("error.html", message=message)
